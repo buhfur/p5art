5fa678768ef1fe72e7b4e42d39088d54a2317e59
tvhead <ryanmcvicker12@gmail.com>
2022-02-15 11:21:12 -0500

    changes were made

--- js/draw.js
+++ js/draw.js
@@ -1,13 +1,32 @@
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(1320, 1000);
+
+	rect(10,40,40,10);
+
 }
 
+
+//function granulate(float x){ }
+
+
+
 function draw() {
-	if (mouseIsPressed) {
-		fill(0);
-	} else {
-		fill(255);
+	//quad(38, 31, 86, 20, 69, 63, 30, 76);
+	const args = [];
+	//push 8 points through these functions 
+	for(let x = 0 ; x < 8 ; x++){
+		//perform f(x) = x ** -1 
+		args.push( x ** 2) ; 	
 	}
-	ellipse(mouseX, mouseY, 80, 80);
+	
+	for ( let i = 0 ; i < 10 ; i++) {
+		for(let j = 0 ; j < args.length; j++ ) 
+			args[j] *= 2; 
+		
+
+		quad.apply(this,args);
+
+	}
 }
+
